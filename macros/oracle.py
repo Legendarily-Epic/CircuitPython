0886aaaad644d4b692b1c378ae680d9a1d7e8d2f
Rick C. Tompkins <68399569+Legendarily-Epic@users.noreply.github.com>
2021-10-29 20:39:27 -0700

    Updated

--- macros/oracle.py
+++ macros/oracle.py
@@ -7,20 +7,20 @@ app = {                       # REQUIRED dict, must be named 'app'
     'macros' : [              # List of button macros...
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
-        (0x400000, 'Login', ['rick.tompkins', Keycode.TAB, 'Rct.031293\n']),
-        (0x400000, 'Pass', ['Rct.031293\n']),
-        (0x000000, '', ['rick.tompkins', Keycode.TAB, 'Rct.031293\n']),
+        (0x400000, 'Login', ['OracleID', Keycode.TAB, 'OraclePass\n']),
+        (0x400000, 'Pass', ['OraclePass\n']),
+        (0x000000, '', ['']),
         # 2nd row ----------
         (0x000040, 'OnHand', [Keycode.TAB, -Keycode.TAB, Keycode.TAB, -Keycode.TAB, Keycode.TAB, -Keycode.TAB, Keycode.TAB, -Keycode.TAB, Keycode.TAB, -Keycode.TAB,
                                 '1', Keycode.TAB, -Keycode.TAB, '999999\n', 5.0, Keycode.ALT, -Keycode.ALT, Keycode.DOWN_ARROW, -Keycode.DOWN_ARROW,
                                 Keycode.DOWN_ARROW, -Keycode.DOWN_ARROW, Keycode.DOWN_ARROW, -Keycode.DOWN_ARROW, Keycode.DOWN_ARROW, -Keycode.DOWN_ARROW,
                                 Keycode.DOWN_ARROW, -Keycode.DOWN_ARROW, Keycode.DOWN_ARROW, -Keycode.DOWN_ARROW, Keycode.ENTER, -Keycode.ENTER, 8.0, Keycode.TAB,-Keycode.TAB,
                                 Keycode.TAB,-Keycode.TAB, 0.5, Keycode.ENTER]),
-        (0x000000, '', ['rick.tompkins']),
-        (0x000000, '', ['rick.tompkins']),
+        (0x000000, '', ['']),
+        (0x000000, '', ['']),
         # 3rd row ----------
-        (0x000000, '', ['XSW@3edcvb\n']),
-        (0x000000, '', ['Rct.031293\n']),
+        (0x000000, '', ['']),
+        (0x000000, '', ['']),
         (0x002000, 'FIND', [Keycode.ALT, -Keycode.ALT, Keycode.RIGHT_ARROW, -Keycode.RIGHT_ARROW, Keycode.RIGHT_ARROW, -Keycode.RIGHT_ARROW, Keycode.DOWN_ARROW, -Keycode.DOWN_ARROW, Keycode.DOWN_ARROW, -Keycode.DOWN_ARROW, Keycode.DOWN_ARROW, -Keycode.DOWN_ARROW, Keycode.ENTER]),
         # 4th row ----------
         (0x202000, 'SHPPRT', ['INT_PHX_SHIPPING3', Keycode.TAB, -Keycode.TAB]),

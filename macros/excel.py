cd9f820f769dedf1495f6af4a57a5977dfb7e706
Rick C. Tompkins <68399569+Legendarily-Epic@users.noreply.github.com>
2021-10-29 20:37:00 -0700

    Updated

--- macros/excel.py
+++ macros/excel.py
@@ -8,21 +8,19 @@ app = {                       # REQUIRED dict, must be named 'app'
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
         (0x400000, 'ClrFtr', [Keycode.ALT, -Keycode.ALT, 0.5, 'h', 0.5, 's', 0.5, 'c', 0.5, Keycode.ENTER, -Keycode.ENTER]),
-        (0x000000, '', ['rick.tompkins', Keycode.TAB,
-                           'Rct.031293\n']),
-        (0x000000, '', ['rick.tompkins', Keycode.TAB,
-                           'Rct.031293\n']),
+        (0x000000, '', ['']),
+        (0x000000, '', ['']),
         # 2nd row ----------
-        (0x000000, '', ['e400646']),
-        (0x000000, '', ['rick.tompkins']),
-        (0x000000, '', ['rick.tompkins']),
+        (0x000000, '', ['']),
+        (0x000000, '', ['']),
+        (0x000000, '', ['']),
         # 3rd row ----------
-        (0x000000, '', ['XSW@3edcvb\n']),
-        (0x000000, '', ['Rct.031293\n']),
-        (0x000000, '', ['Rct.031293\n']),
+        (0x000000, '', ['']),
+        (0x000000, '', ['']),
+        (0x000000, '', ['']),
         # 4th row ----------
         (0x202000, '<Sheet', [Keycode.CONTROL, Keycode.PAGE_UP]),
-        (0x000000, '', [Keycode.ALT, -Keycode.ALT, Keycode.RIGHT_ARROW, -Keycode.RIGHT_ARROW, Keycode.RIGHT_ARROW, -Keycode.RIGHT_ARROW, Keycode.DOWN_ARROW, -Keycode.DOWN_ARROW, Keycode.DOWN_ARROW, -Keycode.DOWN_ARROW, Keycode.DOWN_ARROW, -Keycode.DOWN_ARROW, Keycode.ENTER]),
+        (0x000000, '', ['']),
         (0x202000, 'Sheet>', [Keycode.CONTROL, Keycode.PAGE_DOWN]),
         # Encoder button ---
         (0x000000, '', [Keycode.WINDOWS, 'l']) # Lock Computer
